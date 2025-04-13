8f62d4553411596acd1d27a7a184ce3598df92d3
tjhhh <muhammadfauzan292004@gmail.com>
2025-04-13 10:17:26 +0700

    menyelesaikan program dengan runtime configuration

---
+++ tpmod8_103022300065/CovidConfig.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace tpmod8_103022300065
+{
+    class CovidConfig
+    {
+        public string satuan_suhu { get; set; } = "celcius";
+        public int batas_hari_deman { get; set; } = 14;
+        public string pesan_ditolak { get; set; } = "Anda tidak diperbolehkan masuk ke dalam gedung ini";
+        public string pesan_diterima { get; set; } = "Anda dipersilahkan untuk masuk ke dalam gedung ini";
+        public static CovidConfig LoadConfig(string path)
+        {
+            if (File.Exists(path))
+            {
+                string json = File.ReadAllText(path);
+                var config = JsonSerializer.Deserialize<CovidConfig>(json);
+
+                // Assign default jika masih CONFIG1-4
+                if (config.satuan_suhu == "CONFIG1") config.satuan_suhu = "celcius";
+                if (config.batas_hari_deman == 0 || config.batas_hari_deman.ToString() == "CONFIG2") config.batas_hari_deman = 14;
+                if (config.pesan_ditolak == "CONFIG3") config.pesan_ditolak = "Anda tidak diperbolehkan masuk ke dalam gedung ini";
+                if (config.pesan_diterima == "CONFIG4") config.pesan_diterima = "Anda dipersilahkan untuk masuk ke dalam gedung ini";
+
+                return config;
+            }
+
+            // fallback default
+            return new CovidConfig();
+        }
+
+        public void UbahSatuan()
+        {
+            if (satuan_suhu == "celcius")
+                satuan_suhu = "fahrenheit";
+            else
+                satuan_suhu = "celcius";
+        }
+    }
+}
--- tpmod8_103022300065/Program.cs
+++ tpmod8_103022300065/Program.cs
@@ -1,2 +1,56 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using tpmod8_103022300065;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        CovidConfig config = CovidConfig.LoadConfig("covid_config.json");
+
+        Console.Write($"Berapa suhu badan anda saat ini? (dalam {config.satuan_suhu}): ");
+        double suhu = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Berapa hari yang lalu (perkiraan) anda terakhir memiliki gejala demam? ");
+        int hari = Convert.ToInt32(Console.ReadLine());
+
+        double batasMin, batasMax;
+
+        if (config.satuan_suhu.ToLower() == "celcius")
+        {
+            batasMin = 36.5;
+            batasMax = 37.5;
+        }
+        else
+        {
+            batasMin = 97.7;
+            batasMax = 99.5;
+        }
+
+        if (suhu >= batasMin && suhu <= batasMax && hari < Convert.ToInt32(config.batas_hari_deman))
+        {
+            Console.WriteLine(config.pesan_diterima);
+        }
+        else
+        {
+            Console.WriteLine(config.pesan_ditolak);
+        }
+
+        // Panggil UbahSatuan di akhir
+        config.UbahSatuan();
+        Console.WriteLine($"Satuan suhu sekarang diubah menjadi: {config.satuan_suhu}");
+        double suhuSetelahKonversi;
+
+        if (config.satuan_suhu == "fahrenheit")
+        {
+            suhuSetelahKonversi = (suhu * 9 / 5) + 32;
+        }
+        else
+        {
+            suhuSetelahKonversi = (suhu - 32) * 5 / 9;
+        }
+
+        Console.WriteLine($"Suhu badan anda setelah dikonversi: {suhuSetelahKonversi:F1}° {config.satuan_suhu}");
+
+    }
+}
+
